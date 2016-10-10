dbaa0ad56ed0ec4fd42a88f0ed470b968da04b5d
incleaf <incleaf@gmail.com>
2016-10-10 12:59:17 +0900

    Add blankslate in playlist

--- frontend/src/components/Playlist.js
+++ frontend/src/components/Playlist.js
@@ -9,9 +9,20 @@ class Playlist extends React.Component {
         <div className="playlist__header">
           <h2 className="playlist__title">Playlist</h2>
         </div>
-        <ul>
-          {data.map(item =>
-            <li key={item.id}>{item.title}</li>)
+        <ul className="playlist__content">
+          {
+            data.length
+            ? data.map(item =>
+              <li key={item.id}>{item.title}</li>
+            )
+            : <div className="playlist__blankslate">
+              <h3 className="playlist__blankslate__title">
+                Playlist is empty
+              </h3>
+              <p className="playlist__blankslate__subtitle">
+                Why don't you add a music here?
+              </p>
+            </div>
           }
         </ul>
       </div>
