67788cff81458ee2909a6639104d7e0613b82f2f
incleaf <incleaf@gmail.com>
2016-10-31 13:04:55 +0900

    Update param to get saerch result at once

--- frontend/src/utils/youtube.js
+++ frontend/src/utils/youtube.js
@@ -11,47 +11,16 @@ const fetch = axios.create({
 });
 
 // API Documentation: https://developers.google.com/youtube/v3/docs/search/list
-export async function searchVideos(query) {
-  let result = [];
-  let pageToken;
-
-  for (let searchCount = 0; searchCount < 5; searchCount++) {
-    const res = await fetch.get('search', {
-      params: {
-        // Skip `q` param to get next data with pageToken
-        q: searchCount === 0 ? query : null,
-        part: 'snippet',
-        type: 'video',
-        key: API_KEY,
-        pageToken,
-      },
-    }).then(res => res.data);
-
-    if (searchCount === 0 && res.items.length === 0) {
-      break;
-    }
-
-    result = [...result, ...res.items];
-
-    if (!res.hasOwnProperty('nextPageToken')) {
-      break;
-    }
-
-    pageToken = res.nextPageToken;
-  }
-
-  return result;
-
-  // for (let i = 1; i < searchCount; i++) {
-  //   await fetch.get('search', {
-  //     params: {
-  //       pageToken,
-  //       part: 'snippet',
-  //       type: 'video',
-  //       key: API_KEY,
-  //     }
-  //   }).then(res => )
-  // }
+export function searchVideos(query) {
+  return fetch.get('search', {
+    params: {
+      q: query,
+      part: 'snippet',
+      type: 'video',
+      key: API_KEY,
+      maxResults: 25,
+    },
+  }).then(res => res.data.items);
 }
 
 export function youtubeTimeWatcher(player, cb) {
