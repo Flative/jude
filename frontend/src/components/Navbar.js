a7ca301a459e704af511e42e89f797981fddfb97
Hyeonsu Lee <incleaf@gmail.com>
2016-12-04 22:52:46 +0900

    Update mode selector popup style

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,39 +11,37 @@ class NavBar extends React.Component {
         </div>
         <div className="navbar__mode-selector">
           <p className="title">Change Mode</p>
-          <hr />
+          {/*<hr />*/}
 
-          <div className="form-group">
-            <input type="text" />
+          <div className="form-group form-group--text">
+            <div className="form-group__title">
+              Server Address
+            </div>
+            <div className="form-group__body">
+              <input type="text" />
+            </div>
           </div>
 
-          <div className="form-group form-group__mode">
-            <label className="radio-label" htmlFor="hostRadio">
-              <input type="radio" name="toggle" value="host" id="hostRadio" />
-              <span>Host</span>
-            </label>
-            <label className="radio-label" htmlFor="clientRadio">
-              <input type="radio" name="toggle" value="client" id="clientRadio" />
-              <span>Client</span>
-            </label>
-          </div>
-
-
-          <div className="form-group">
+          <div className="form-group form-group--type">
             <div className="form-group__title">
               Type
             </div>
             <div className="form-group__body">
-
+              <label className="radio-label" htmlFor="hostRadio">
+                <input type="radio" name="toggle" value="host" id="hostRadio" />
+                <span>Host</span>
+              </label>
+              <label className="radio-label" htmlFor="clientRadio">
+                <input type="radio" name="toggle" value="client" id="clientRadio" />
+                <span>Client</span>
+              </label>
+              <div className="clearfix"></div>
             </div>
           </div>
-          <div className="form-group">
-            <div className="form-group__title">
-              Server Address
-            </div>
-            <div className="form-group__item">
-              <input type="text" />
-            </div>
+
+          <div className="buttons">
+            <button className="button">Cancel</button>
+            <button className="button button--apply">Apply</button>
           </div>
         </div>
       </div>
