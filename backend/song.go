a72195de74e2111eeeaecd0b44c25266fb5eb587
leejaedus <leejaeduss@gmail.com>
2016-12-05 21:35:00 +0900

    Add json tag to `Song` and fix typo

--- backend/song.go
+++ backend/song.go
@@ -1,8 +1,8 @@
 package main
 
 type Song struct {
-	ID    string
-	Title string
-	UUID  string
-	index int
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	UUID  string `json:"uuid"`
+	Index int    `json:"index"`
 }
