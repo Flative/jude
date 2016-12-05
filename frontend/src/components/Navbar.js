eeb39c9cca583902396f4dc061010945115d172a
Hyeonsu Lee <incleaf@gmail.com>
2016-12-05 22:01:59 +0900

    Update style on mode selector

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,24 +1,64 @@
 import React from 'react'
+import classNames from 'classnames';
 
 class NavBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isSelectorOpened: false,
+    }
+    this.updateSwitch = this.updateSwitch.bind(this)
+  }
+
+  // TODO: Animation on mode selector
+  updateSwitch(isSelectorOpened) {
+    this.setState({ isSelectorOpened })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (!prevState.isSelectorOpened && this.state.isSelectorOpened) {
+      this.refs.serverAddressInput.focus();
+    }
+  }
+
   render() {
+    const { isSelectorOpened } = this.state
+    const modeSelectorClass = classNames([
+      'navbar__mode-selector',
+      { 'navbar__mode-selector--active': isSelectorOpened }
+    ])
+    const appCoverClass = classNames(['app-cover', { 'app-cover--active': isSelectorOpened }])
+
     return (
       <div className="navbar">
         <h1 className="navbar__title">Jude</h1>
         <div className="material-switch navbar__switch">
-          <input id="navbarSwitch" type="checkbox" />
+          <input
+            id="navbarSwitch"
+            type="checkbox"
+            checked={isSelectorOpened}
+            onChange={() => this.updateSwitch(!isSelectorOpened)}
+          />
           <label htmlFor="navbarSwitch" />
         </div>
-        <div className="navbar__mode-selector">
+        <div className={appCoverClass} onClick={() => this.updateSwitch(!isSelectorOpened)}/>
+        <div
+          className={modeSelectorClass}
+          tabIndex="1"
+          onKeyDown={(e) => {
+            if (e.keyCode == 27) {
+              this.updateSwitch(!isSelectorOpened)
+            }
+          }}
+        >
           <p className="title">Change Mode</p>
-          {/*<hr />*/}
 
           <div className="form-group form-group--text">
             <div className="form-group__title">
               Server Address
             </div>
             <div className="form-group__body">
-              <input type="text" tabIndex="1" />
+              <input type="text" tabIndex="1" ref="serverAddressInput" />
             </div>
           </div>
 
@@ -40,7 +80,13 @@ class NavBar extends React.Component {
           </div>
 
           <div className="buttons">
-            <button className="button" tabIndex="5">Cancel</button>
+            <button
+              className="button"
+              tabIndex="5"
+              onClick={() => this.updateSwitch(false)}
+            >
+              Cancel
+            </button>
             <button className="button button--apply" tabIndex="4">Apply</button>
           </div>
         </div>
