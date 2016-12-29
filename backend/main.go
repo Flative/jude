6bc348e97f7e298d26fa7a168046d14eb1b23e03
leejaedus <leejaeduss@gmail.com>
2016-12-30 00:05:54 +0900

    Group business logic to `Service`

--- backend/main.go
+++ backend/main.go
@@ -1,28 +1,10 @@
 package main
 
-import (
-	"flag"
-	"html/template"
-	"net/http"
-
-	"log"
-)
-
 var (
-	addr         = flag.String("addr", "127.0.0.1:5050", "http service address")
-	homeTemplate = template.Must(template.ParseFiles("static/index.html"))
+	options = map[string]interface{}{"host": "127.0.0.1", "port": 5050}
+	service = NewService(options)
 )
 
 func main() {
-	manager := newManager()
-	log.Printf("\n\n* Running on\n* ws://%s/ws\n* http://%s/\n\n(Press CTRL+C to quit)\n", *addr, *addr)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		homeTemplate.Execute(w, r.Host)
-	})
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWs(manager, w, r)
-	})
-	log.Fatal(http.ListenAndServe(*addr, nil))
+	service.Run()
 }
---
+++ backend/service.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type Service struct {
+	host string
+	port int
+	addr string
+}
+
+func NewService(options map[string]interface{}) Service {
+	service := Service{}
+
+	if host, ok := options["host"].(string); ok {
+		service.host = host
+	} else {
+		service.host = "127.0.0.1"
+	}
+
+	if port, ok := options["port"].(float64); ok {
+		service.port = int(port)
+	} else {
+		service.port = 5050
+	}
+
+	service.addr = service.host + ":" + strconv.Itoa(service.port)
+
+	return service
+}
+
+func (s *Service) Run() {
+	var (
+		addr         = flag.String("addr", s.addr, "http service address")
+		homeTemplate = template.Must(template.ParseFiles("static/index.html"))
+	)
+
+	manager := newManager()
+	log.Printf("\n\n* Running on\n* ws://%s/ws\n* http://%s/\n\n(Press CTRL+C to quit)\n", *addr, *addr)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		homeTemplate.Execute(w, r.Host)
+	})
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(manager, w, r)
+	})
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
