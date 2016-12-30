94b3265078a800d37943b02152b0fe3d3475516f
leejaedus <leejaeduss@gmail.com>
2016-12-30 21:59:53 +0900

    Update `Song` test code

--- backend/song_test.go
+++ backend/song_test.go
@@ -1,40 +1,87 @@
 package main
 
 import (
+	"encoding/json"
 	"testing"
 )
 
 func TestSongValidate(t *testing.T) {
-	song := newSong()
+	var song *Song
 	var validationResult bool
+	var rawData []byte
 
-	validationResult = song.validate()
-	if validationResult {
-		t.Errorf("%t should be false", validationResult)
+	rawData = []byte(`{
+		"id": "2qVUvnnbsJk",
+		"title": "This is title of song",
+		"uuid": "705999e4-4c5c-4258-bee0-501eb0a27b3a",
+		"index": 3
+	}`)
+	song = newSong()
+	if err := json.Unmarshal(rawData, &song); err != nil {
+		t.Errorf("Every raw data should be parsed")
 	}
-
-	song.ID = "ecFZkVn4iW4"
-	validationResult = song.validate()
-	if validationResult {
-		t.Errorf("%t should be false", validationResult)
-	}
-
-	song.Title = "This is title of song"
-	validationResult = song.validate()
-	if validationResult {
-		t.Errorf("%t should be false", validationResult)
-	}
-
-	song.UUID = "705999e4-4c5c-4258-bee0-501eb0a27b3a"
-	validationResult = song.validate()
-	if validationResult {
-		t.Errorf("%t should be false", validationResult)
-	}
-
-	song.Index = 1
 	validationResult = song.validate()
 	if !validationResult {
 		t.Errorf("%t should be true", validationResult)
 	}
 
+	rawData = []byte(`{
+		"id": "",
+		"title": "This is title of song",
+		"uuid": "705999e4-4c5c-4258-bee0-501eb0a27b3a",
+		"index": 3
+	}`)
+	song = newSong()
+	if err := json.Unmarshal(rawData, &song); err != nil {
+		t.Error("Invalid JSON")
+	}
+	validationResult = song.validate()
+	if validationResult {
+		t.Errorf("%t should be false", validationResult)
+	}
+
+	rawData = []byte(`{
+		"id": "2qVUvnnbsJk",
+		"title": "",
+		"uuid": "705999e4-4c5c-4258-bee0-501eb0a27b3a",
+		"index": 3
+	}`)
+	song = newSong()
+	if err := json.Unmarshal(rawData, &song); err != nil {
+		t.Errorf("Every raw data should be parsed")
+	}
+	validationResult = song.validate()
+	if validationResult {
+		t.Errorf("%t should be false", validationResult)
+	}
+
+	rawData = []byte(`{
+		"id": "2qVUvnnbsJk",
+		"title": "This is title of song",
+		"uuid": "",
+		"index": 3
+	}`)
+	song = newSong()
+	if err := json.Unmarshal(rawData, &song); err != nil {
+		t.Errorf("Every raw data should be parsed")
+	}
+	validationResult = song.validate()
+	if validationResult {
+		t.Errorf("%t should be false", validationResult)
+	}
+
+	rawData = []byte(`{
+		"id": "2qVUvnnbsJk",
+		"title": "This is title of song",
+		"uuid": "705999e4-4c5c-4258-bee0-501eb0a27b3a",
+		"index": -1
+	}`)
+	song = newSong()
+	if err := json.Unmarshal(rawData, &song); err != nil {
+		t.Errorf("Every raw data should be parsed")
+	}
+	validationResult = song.validate()
+	if validationResult {
+		t.Errorf("%t should be false", validationResult)
+	}
 }
